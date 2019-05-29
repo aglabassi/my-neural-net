5f264b70579df6d51af4e945b1a76c13b156483b
Abdel Ghani Labassi <abdel.ghani.labassi@umontreal.ca>
2019-05-29 15:13:45 +0100

    Add files via upload

---
+++ NN.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 29 15:07:46 2019
+
+@author: Abdel
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 27 10:32:21 2019
+
+@author: Abdel Ghani Labassi
+Inspired from Andrew NG's lectures on ML
+"""
+import numpy as np
+from utils import *
+
+#An implementation of neural network learning algorithm using crossentropy cost
+#function and gradient descent for its minimization. Gradient is computed according
+#to the backpropagation algorithm
+class NeuralNetwork:
+  
+    def __init__(self, hiden_layers_sizes ):
+        
+        self.hiden_layers_sizes = hiden_layers_sizes 
+        
+        self.nlayers = len(hiden_layers_sizes) + 2 #io layers
+      
+    #returns the activations given the forward prop. of input x until the lth
+    #layer, including the lth layer, and including inputs activation. We also 
+    #include bias units 's output, wich is 1 for every layer.
+    def _forward_propagate(self, x, l):
+        
+        ai,i = np.concatenate(([1], x)) , 0
+        activations = [ ai ]
+        
+        #Forward propagation
+        while i < l:
+            
+            ai = sigmoid( np.dot(self.W[i], ai) )
+            ai =  np.concatenate(([1],ai) )
+            
+            activations.append( ai )
+            i = i + 1 
+        
+        return activations
+    
+
+    #Computes Djdi of each neurons n. DJdi is the derivative of J in respect 
+    #of the inputs of each neuron n, stocked a natural list of list format
+    #We define "input of a neuron" as the things that comes to it activation function,
+    #We define "output of a neuron" as it activation.
+    # We similarly define "input/output of a layer"
+    def _compute_DJdis(self, activations, y):
+        
+        res = [0]*self.nlayers  #No error in layer 0, so res[0] 
+        res[-1] = np.array( activations[-1][1:] - y )
+        for i in range(self.nlayers - 2, 0, -1):
+            
+            #Derivative of error according to output of current layer, backpropaging
+            #throw the arcs, ignoring bias unit because bias unit doens have entering arcs
+            dJdo  = np.transpose( np.dot(np.transpose(self.W[i]), res[i+1] ))[1:]
+            
+            #Derivative of output according to inputs relative to the current layer.
+            dodi = dsigmoid(np.dot(self.W[i-1], activations[i-1]))
+            
+            res[i] = dJdo * dodi
+        
+        return  res  
+                 
+    # Computes gradient of cost Function given dataset using backprop
+    #return result in a list of matrix in same format as W for convenciency
+    #i.e D[l][j][i] contains derivative for the weigth associated with 
+    #edge ij in layer l.
+    def _gradJ(self,X,Y,regularization_parameter ):
+        
+        m = len(X)
+        acc = [ np.zeros(w.shape) for w in self.W ] #acc is used to compute gradJ 
+        for i in range(m):
+            
+            acts = self._forward_propagate( X[i], self.nlayers - 1 )
+            
+            all_dJdis = self._compute_DJdis(acts, Y[i])
+            
+            #Upgrade acc
+            for l in range(len(acc)) :
+                
+                dJdi = np.transpose( [all_dJdis[l+1]] )
+                
+                didw = [ acts[l] ]
+                
+                acc[l] = acc[l] + np.matmul( dJdi, didw )
+    
+        #Form gradient by normalizing acc
+        acc_normalyzer = lambda l: lambda i,j: acc[l][i,j] / len(X) + \
+                                        bool(j) * regularization_parameter * self.W[l][i,j]
+        
+        
+        #We stock the partial derivative in a datastructure of exact same format as self.W
+        
+        gradJ = [ form_matrix( acc_normalyzer(l), acc[l].shape ) for l in range( len(acc) ) ] 
+        
+        return gradJ
+    
+    
+    
+    def fit(self,X,Y, learning_rate = 0.5, regularization_parameter = 0.0005, epsilon = 0.05):
+        
+        #Y's format normalisation
+        try:
+            output_dim = len(Y[0])
+            
+        except:
+            output_dim = 1
+            Y = [ [y] for y in Y]
+        
+        # sizes[l] gives the number of neurons in layer l.
+        sizes = [ len(X[0]) ] + list(self.hiden_layers_sizes) + [ len(Y[0]) ]
+        
+        
+        #W[l][i,j] gives weigth of the arc (j,i), node i being in layer
+        #number l, input layer being layer number 0.
+        self.W = [  np.random.rand( int(sizes[l+1]), int(sizes[l] + 1)  ) 
+                    for l in range( self.nlayers - 1 ) ]
+        
+        
+        while( True ):
+            
+            error = np.abs(self._J(X,Y,regularization_parameter))
+            
+            if error < epsilon:
+                break
+            
+            gradJ = self._gradJ(X, Y, regularization_parameter)
+            
+            #Gradient descent
+            for i in range(len(self.W)): 
+                
+                    self.W[i] = self.W[i] - learning_rate*gradJ[i]
+                    
+                
+            
+                        
+               
+    def predict(self,X):
+        
+        return np.array([ 1 if self._output(x) > 0.5 else 0 for x in X ])
+    
+    #Compute the output of the neural net given an input x
+    def _output(self,x):
+        
+        return self._forward_propagate( x, self.nlayers - 1  )[-1][1:]
+    
+    
+    #Cost function : Evaluates the quality of the parameters depending on the 
+    #training examples    
+    def _J( self,X,Y,regularization_parameter ):
+        
+        dataset_size = len(X)
+    
+            
+        #cost for a single coordinate of the output
+        cost1D = lambda oj,yj: -( yj * np.log( oj ) + (1 - yj) * np.log( 1-oj ) )    
+        
+        
+        fitting_term =0
+        for i in range(dataset_size):
+            
+            output_i, target_i = self._output( X[i] ), Y[i]
+            
+            for j in range(len(Y[0])):   
+                
+                fitting_term = fitting_term + cost1D( output_i[j],target_i[j] )
+        
+        fitting_term = fitting_term / dataset_size 
+        
+        #We dont include biases in reg. term
+        regularization_term = 0
+        for l in range(len(self.W)):
+            
+            regularization_term =  np.sum(self.W[l][:,1:])
+        
+        
+        regularization_term = regularization_parameter * regularization_term / (2*dataset_size)
+        
+        
+        return fitting_term + regularization_term
+    
+    
+
+nn = NeuralNetwork([2])
+
+X_train, X_test, y_train, y_test = gen_dataset( np.logical_xor, 2 )
+
+X, y = X_train + X_test, y_train + y_test
+
+nn.fit( X_train, y_train, epsilon = 0.05 , learning_rate = 0.5 , regularization_parameter = 0.0005 )
+
+print( np.mean( a.predict(X) == y ) )
---
+++ utils.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 29 13:57:20 2019
+Contains various useful functions
+
+@author: Abdel
+"""
+import numpy as np
+
+#Form a matrix M where M[i,j] == fc(i,j). 
+#(fc is a 2-parameter function)
+def form_matrix(fc, shape):
+    
+    assert(len(shape) == 2)
+    
+    res = np.zeros(shape)
+    
+    for i in range(shape[0]):      
+        for j in range(shape[1]):
+            
+            res[i,j] = fc(i,j)
+            
+    return res
+
+#Assert fc is logical binary fonction. Generate a training/test set according to
+# fc and sep    
+def gen_dataset(fc, input_dim, sep = 0.8):
+    
+    X =  [ [ int(s) for s in np.binary_repr(i,input_dim) ] for i in range(2**input_dim)  ]
+    
+    Xpos,Xneg = [],[]
+    ypos, yneg = [], []
+    
+    for x in X:
+        y = fc.reduce(x)
+        if y:
+            ypos.append(y)
+            Xpos.append(x)
+            
+        else:
+            yneg.append(y)
+            Xneg.append(x)
+            
+    tpos, tneg = int(np.around(sep*len(Xpos))), int(np.around(sep*len(Xneg)))
+    X_train, y_train = Xpos[:tpos] +  Xneg[:tneg], ypos[:tpos] + yneg[:tneg]
+    X_test, y_test = Xpos[tpos:] + Xneg[tneg:], ypos[tpos:] + yneg[tneg:]
+    
+    return X_train, X_test, y_train, y_test
+    
+def dsigmoid(z):
+    
+    return sigmoid(z)*(1-sigmoid(z))
+ 
+def sigmoid(z):
+    
+    return 1/(1+np.exp(-z))
